82944a0eb49e7fdfa20fb54d2c6cda98ad39f500
Abdullah Ergül <45385505+abdullah-ergul@users.noreply.github.com>
2022-03-17 23:03:31 +0300

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -1,5 +1,4 @@
-# C_Programming_Language
->> A Github file, Programming Language 1 - 2 courses which are my first year B.Sc. degree courses at Karabuk University.
+A Github file for Programming Language 1 - 2 courses which are my 1st year B.Sc. degree courses at Karabuk University.
 >> 1st semester teacher: Emrah OZKAYNAK | Lab Asistant: Mehmet Zaid YILDIRIM
 >> 2nd semester teacher: Emrah OZKAYNAK | Lab Asistant: 
 >> In 2nd semester I took extra lessons from Caner Ozcan
