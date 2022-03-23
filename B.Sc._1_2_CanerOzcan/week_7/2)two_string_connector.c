69e147921faa72d487ad11e6529f0953f363426c
Abdullah Ergül <abdullahergul23@gmail.com>
2022-03-23 15:54:57 +0300

    Upload From PC

---
+++ B.Sc._1_2_CanerOzcan/week_7/2)two_string_connector.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char *a= (char*)calloc(1, sizeof(char));
+	char *b= (char*)calloc(1, sizeof(char));
+	
+	
+	
+	return 0;
+}
---
+++ B.Sc._1_2_EmrahOzkaynak/week_7/5)reverse_sentence_printer .c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	int i,j=0,k;
+	char *a= (char*)calloc(50, sizeof(char));
+	char *b= (char*)calloc(10, sizeof(char));
+	
+	gets(a);
+	
+	for(i=strlen(a)-1; i>=0; i--){
+		if(*(a+i) == ' '){
+			for(k=j-1;k>=0;k--){
+				printf("%c",*(b+k));
+			}
+			printf("\n");
+			j=0;
+		}
+		*(b+j)= *(a+i);
+		j++;
+	}
+	
+	return 0;
+}
