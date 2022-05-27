49362e0b20c595726a038156d766b7bdabd617c3
Abdullah Ergül <abdullahergul23@gmail.com>
2022-05-27 04:23:18 +0300

    Update From PC

---
+++ B.Sc._1_2_CanerOzcan/week_11-Random_Access_Files/1)Random_Access_Files.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct Student{
+    int num; char name[20]; char department[20];
+};
+typedef struct Student Student;
+
+int chooseFunc(void);
+void mkFile(void);
+
+
+int main(){
+
+
+    return 0;
+}
--- B.Sc._1_2_EmrahOzkaynak/week_11-Random_Access_Files/1)Random_Access_Files.c
+++ B.Sc._1_2_EmrahOzkaynak/week_11-Random_Access_Files/1)Random_Access_Files.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 
 struct customer{
 int accnum; char name[20]; char sname[25]; double deposit;
@@ -7,6 +8,7 @@ int accnum; char name[20]; char sname[25]; double deposit;
 
 int chooseFunc(void);
 void mkFile(void);
+void listFile(void);
 void listIndebtedAccounts(void);
 void findMaxDeposit(void);
 void transferRecords(void);
@@ -14,56 +16,88 @@ void updateRecords(void);
 
 
 int main(){
-    int chooice = chooseFunc();
+    int choice = chooseFunc();
 
     while(1){
-        if(chooice == 1)
+        if(choice == 1)
             mkFile();
-        else if(chooice == 2)
+        else if(choice == 2)
+            listFile();
+        else if(choice == 3)
             listIndebtedAccounts();
-        else if(chooice == 3)
+        else if(choice == 4)
             findMaxDeposit();
-        else if(chooice == 4)
+        else if(choice == 5)
             transferRecords();
-        else if(chooice == 5)
+        else if(choice == 6)
             updateRecords();
-        else if(chooice == 0)
+        else if(choice == 0)
             break;
         else
             printf("Your Choice is Not Found! Please Enter a Diffrent Number.");
 
-        chooice = chooseFunc();
+        choice = chooseFunc();
     }
 
     return 0;
 }
 
 int chooseFunc(void){
-    int chooice;
+    int choice;
     printf("1 for Create a File as 'customer.dat' (If There Is a File With This Name, That File Is Deleted)\n");
-    printf("2 for List Customers With a Balance Greater Than '0'\n");
-    printf("3 for Show Maximum Balance Account\n");
-    printf("4 for Create a File as 'list.txt' to Account's for List Balance Account's Greater Than '300'\n");
-    printf("5 for Update Balances whose Customer's Have Balance Greater Than '300'\n");
+    printf("2 for List Customers\n");
+    printf("3 for List Customers With a Balance Greater Than '0'\n");
+    printf("4 for Show Maximum Balance Account\n");
+    printf("5 for Create a File as 'list.txt' to Account's for List Balance Account's Greater Than '300'\n");
+    printf("6 for Update Balances whose Customer's Have Balance Greater Than '300'\n");
     printf("0 for Exit Program\n");
-    printf("\nYour Choice: "); scanf("%d",&chooice);
+    printf("\nYour Choice: "); scanf("%d",&choice);
+    printf("\n");
 
-    return chooice;
+    return choice;
 }
 
 void mkFile(void){
-    int i;
+    int i, fsize;
     FILE *fptr;
     customer empty= {0, "", "", 0.0};
 
     if((fptr= fopen("customer.dat","w")) == NULL)
-        printf("File Can't Create!\n");
+        printf("Can't Create File!\n");
     else{
-        for(i=0; i<20; i++){
+        printf("Enter Customer Count: "); scanf("%d", &fsize);
+
+        for(i=1; i<fsize; i++){
             fwrite(&empty, sizeof(customer), 1, fptr);
         }
+        printf("%d Size File Created!\n\n", fsize);
+
+        rewind(fptr);
+        printf("(Enter 0 For Exit)Account Number: "); scanf("%d", &empty.accnum);
+        for(i=1; i<fsize && empty.accnum != 0; i++){
+            printf("Enter Account Informations: "); scanf("%s %s %lf", empty.name, empty.sname, &empty.deposit);
+            fwrite(&empty, sizeof(customer), 1, fptr);
+            printf("\n");
+            printf("(Enter 0 For Exit)Account Number: "); scanf("%d", &empty.accnum);
+        }
+        printf("File Updated!\n\n");
         fclose(fptr);
-        printf("File Created!\n");
+    }
+    printf("\n--------------------------------\n\n");
+}
+
+void listFile(void){
+    FILE *fptr;
+    customer info= {0, "", "", 0.0};
+
+    if((fptr= fopen("customer.dat","r")) == NULL)
+        printf("Can't Find File!\n");
+    else{
+        while(!feof(fptr)){
+            fread(&info, sizeof(customer), 1, fptr);
+            printf("%d %s %s %lf\n", info.accnum, info.name, info.sname, info.deposit);
+        }
+        fclose(fptr);
     }
     printf("\n--------------------------------\n\n");
 }
@@ -74,14 +108,17 @@ void listIndebtedAccounts(void){
     customer info= {0, "", "", 0.0};
 
     if((fptr= fopen("customer.dat","r")) == NULL)
-        printf("File Can't Find!\n");
+        printf("Can't Find File!\n");
     else{
         while(!feof(fptr)){
             fread(&info, sizeof(customer), 1, fptr);
-            if(info.deposit > 0)
+            if(info.deposit > 0){
+                printf("%d %s %s %lf\n", info.accnum, info.name, info.sname, info.deposit);
                 count++;
+            }
         }
-        printf("Count of Customers whose Balance Greater Than '0': %d\n",count);
+        printf("Count of Customers whose Balance Greater Than '0': %d\n", count);
+        fclose(fptr);
     }
     printf("\n--------------------------------\n\n");
 }
@@ -91,7 +128,7 @@ void findMaxDeposit(void){
     customer info= {0, "", "", 0.0}, max= {0, "", "", 0.0};
 
     if((fptr= fopen("customer.dat","r")) == NULL)
-        printf("File Can't Find!\n");
+        printf("Can't Find File!\n");
     else{
         while(!feof(fptr)){
             fread(&info, sizeof(customer), 1, fptr);
@@ -99,15 +136,53 @@ void findMaxDeposit(void){
                 max= info;
         }
         printf("Maximum Balanced Account:\n");
-        printf("%d %c %c %lf\n",max.accnum, max.name, max.sname, max.deposit);
+        printf("%d %s %s %lf\n", max.accnum, max.name, max.sname, max.deposit);
+        fclose(fptr);
     }
     printf("\n--------------------------------\n\n");
 }
 
 void transferRecords(void){
+    FILE *fptr, *fptr2;
+    customer info= {0, "", "", 0.0};
 
+    if((fptr= fopen("customer.dat","r")) == NULL)
+        printf("Can't Find File!\n");
+    else if((fptr2= fopen("list.txt","w")) == NULL)
+        printf("Can't Create File!\n");
+    else{
+        while(!feof(fptr)){
+            fread(&info, sizeof(customer), 1, fptr);
+            if(info.deposit > 300)
+                fprintf(fptr2,"%d %s %s %lf\n",info.accnum, info.name, info.sname, info.deposit);
+        }
+        printf("File Has Been Created!\n");
+        fclose(fptr);
+        fclose(fptr2);
+    }
+    printf("\n--------------------------------\n\n");
 }
 
 void updateRecords(void){
+    int i=0;
+    FILE *fptr;
+    customer info= {0, "", "", 0.0};
 
+    if((fptr= fopen("customer.dat","r+")) == NULL)
+        printf("Can't Find File!\n");
+    else{
+        while(!feof(fptr)){
+            fread(&info, sizeof(customer), 1, fptr);
+            if(info.deposit > 300.0){
+                info.deposit += 50.0;
+                fseek(fptr, i*sizeof(customer), SEEK_SET);
+                fwrite(&info, sizeof(customer), 1, fptr);
+            }
+            Sleep(10);
+            i++;
+        }
+        printf("Process is Successful!\n");
+        fclose(fptr);
+    }
+    printf("\n--------------------------------\n\n");
 }
