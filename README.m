1005d972d7812973a773e49296858350674fcc10
Abdullah Ergül <45385505+abdullah-ergul@users.noreply.github.com>
2022-06-19 14:49:52 +0300

    Update README.m

--- README.m
+++ README.m
@@ -2,3 +2,4 @@ A Github file for Programming Language 1 - 2 courses which are my 1st year B.Sc.
 >> 1st Semester Teacher: Emrah OZKAYNAK | Lab Asistant: Mehmet Zaid YILDIRIM
 >> 2nd Semester Teacher: Emrah OZKAYNAK | Lab Asistant: Saliha OZGUNGOR
 >> In 2nd semester I took extra lessons from Caner Ozcan
+>> At the end of the 2nd semester, I received an assistantship offer from Assoc. Prof. Dr. Emrah Ozkaynak due to my success in the C programming language.
