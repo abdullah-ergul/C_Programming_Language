c0438d96e82306ddbfd6dfa99a912c8cd5602dd8
Abdullah Ergül <abdullahergul23@gmail.com>
2022-07-05 19:34:40 +0300

    Upload From PC

---
+++ Hackerrank/3-Arrays_and_Strings/5-Dynamic_Array_in_C.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* total_number_of_books;
+int** total_number_of_pages;
+
+*(total_number_of_books) = 0;
+**(total_number_of_pages) = 0;
+
+int main(){
+    int total_number_of_shelves;
+    scanf("%d", &total_number_of_shelves);
+    
+    int total_number_of_queries;
+    scanf("%d", &total_number_of_queries);
+    
+    while (total_number_of_queries--) {
+        int type_of_query;
+        scanf("%d", &type_of_query);
+        
+        if (type_of_query == 1) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            // !
+            *(total_number_of_books + x) = (int *)realloc(total_number_of_books+x, )
+            *(total_number_of_books + x) += 1;
+            *(*(total_number_of_pages + x)) += y;
+            // !
+
+        } else if (type_of_query == 2) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            printf("%d\n", *(*(total_number_of_pages + x) + y));
+        } else {
+            int x;
+            scanf("%d", &x);
+            printf("%d\n", *(total_number_of_books + x));
+        }
+    }
+
+    if (total_number_of_books) {
+        free(total_number_of_books);
+    }
+    
+    for (int i = 0; i < total_number_of_shelves; i++) {
+        if (*(total_number_of_pages + i)) {
+            free(*(total_number_of_pages + i));
+        }
+    }
+    
+    if (total_number_of_pages) {
+        free(total_number_of_pages);
+    }
+    
+    return 0;
+}
